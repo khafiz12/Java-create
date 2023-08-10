806701cdb5cde5159b0468f24952f97228f8d691
khafiz12 <karimhafiz7@gmail.com>
2023-08-09 22:31:33 -0400

    Added condition and loop to javascript.
    I added a for loop to pokemanList.

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,33 @@
 
 let pokemonList = [ 
    {Name: 'Charizad', 
-    height: '6feet',   
+    height: 6,   
     types:  ['Firea','Flying'],
     weight:  '200'
 
 },   
 
    { Name: 'Squirtle',
-     height: '2feet',  
+     height: 2,  
      types: ['Water', 'Poisen'],
      weight: '20'
 },
 
     {Name: 'Pikachu',
-    height: '2feet' ,
+    height: 10 ,
     types: ['Electric','Psychic'],
     weight: '13'
 }
-]
+];
+
+for ( let i = 0; i < pokemonList.length; i++ ) {
+    if (pokemonList[i].height > '3' && pokemonList[i].height < '7' )
+    { document.write (  pokemonList[i].Name +   " is medium pokemon "  ) ;}
+     
+    else if (pokemonList[i].height < '3' )
+    {document.write (pokemonList[i].Name +  "  is a small pokemon " );}
+
+    else { document.write (pokemonList[i].Name +  "  WOW That's Big"); }
+}
 
 
